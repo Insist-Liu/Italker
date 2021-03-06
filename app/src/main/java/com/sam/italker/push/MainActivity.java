e6d470002c0b61bb80032962f62c2fdef8a3bf0e
liujunyan <liu_insist@163.com>
2021-03-06 19:17:33 +0800

    init project

---
+++ app/src/main/java/com/sam/italker/push/MainActivity.java
@@ -0,0 +1,12 @@
+package com.sam.italker.push;
+
+import com.sam.italker.R;
+import com.sam.italker.common.app.Activity;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected int getContentLayoutId() {
+        return R.layout.activity_main;
+    }
+}
